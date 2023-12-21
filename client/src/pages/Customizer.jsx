d204513edd9d8c171221eaffdd7d10d236f3ba2c
Pooja Indulkar <98650416+poojaindulkar@users.noreply.github.com>
2023-12-21 22:03:03 +0530

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https:/vesta3d-server.onrender.com/api/v1/dalle', {
+      const response = await fetch('https://vesta3d-server.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
